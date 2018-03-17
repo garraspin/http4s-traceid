8d1c47c1aa3d5df1a76dfa975fb7255d8cea3761
Remko de Jong <aggenebbisj@gmail.com>
2018-03-17 08:16:55 +0100

    Initial commit

---
+++ project/Dependencies.scala
@@ -0,0 +1,21 @@
+import sbt._
+
+object Dependencies {
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val http4s = Seq(
+    "org.http4s"                       %% "http4s-dsl"                  % "0.18.0",
+    "org.http4s"                       %% "http4s-blaze-server"         % "0.18.0",
+    "org.http4s"                       %% "http4s-blaze-client"         % "0.18.0",
+    "org.http4s"                       %% "http4s-circe"                % "0.18.0"
+  )
+  lazy val logging = Seq(
+    "ch.qos.logback"                   %  "logback-classic"             % "1.2.3" % Runtime
+  )
+
+  lazy val monitoring = Seq(
+    "io.kamon"                         %% "kamon-core"                  % "1.1.0",
+    "io.kamon"                         %% "kamon-http4s"                % "1.0.3",
+    "io.kamon"                         %% "kamon-executors"             % "1.0.1",
+    "io.kamon"                         %% "kamon-logback"               % "1.0.0"
+  )
+}
---
+++ src/main/scala/example/Main.scala
@@ -0,0 +1,65 @@
+import java.util.Collections
+import java.util.concurrent.{AbstractExecutorService, TimeUnit}
+
+import cats.effect.IO
+import fs2.StreamApp.ExitCode
+import fs2.{Stream, StreamApp}
+import kamon.executors.util.ContextAwareExecutorService
+import org.http4s.client.blaze.Http1Client
+import org.http4s.dsl.io._
+import org.http4s.server.Router
+import org.http4s.server.blaze.BlazeBuilder
+import kamon.http4s.middleware._
+import org.http4s.HttpService
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutorService}
+
+object Main extends StreamApp[IO] {
+  override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] = {
+    implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(
+      ContextAwareExecutorService(ExecutionContextExecutorServiceBridge(ExecutionContext.global))
+    )
+    for {
+      client <- Http1Client.stream[IO]().map(kamon.http4s.middleware.client.KamonSupport(_))
+      logger  = LoggerFactory.getLogger(this.getClass)
+      router  = Router(
+        "/" -> HttpService[IO] {
+          case GET -> Root / "foo" =>
+            logger.info("foo")
+            client.expect[String]("http://localhost:9000/bar").unsafeRunAsync {
+              case Left(e)  => logger.error("bar failed")
+              case Right(s) => logger.error(s"bar returned $s")
+            }
+            Ok("foo")
+          case GET -> Root / "bar" =>
+            logger.info("bar")
+            Ok("bar")
+        }
+      )
+
+      exitCode <- BlazeBuilder[IO]
+        .bindHttp(9000, "0.0.0.0")
+        .mountService(server.KamonSupport(router))
+        .withExecutionContext(ec)
+        .serve
+    } yield exitCode
+  }
+}
+
+object ExecutionContextExecutorServiceBridge {
+  def apply(ec: ExecutionContext): ExecutionContextExecutorService = ec match {
+    case null => throw null
+    case eces: ExecutionContextExecutorService => eces
+    case other => new AbstractExecutorService with ExecutionContextExecutorService {
+      override def prepare(): ExecutionContext = other
+      override def isShutdown = false
+      override def isTerminated = false
+      override def shutdown() = ()
+      override def shutdownNow() = Collections.emptyList[Runnable]
+      override def execute(runnable: Runnable): Unit = other execute runnable
+      override def reportFailure(t: Throwable): Unit = other reportFailure t
+      override def awaitTermination(length: Long,unit: TimeUnit): Boolean = false
+    }
+  }
+}
