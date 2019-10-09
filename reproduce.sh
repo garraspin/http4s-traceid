a652b4720711a504da5c2522d07b8b8431c58507
Samuel Fernandez Munoz <garraspin00@gmail.com>
2019-10-09 11:23:34 +0200

    Issue with IO.Sleep

--- reproduce.sh
+++ reproduce.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #for i in {1..20}; do curl -H 'X-B3-TraceId: 862ce441e37c8656' -H 'X-B3-SpanId: aa10045d627ffa1d' localhost:9000/foo; done
-#for i in {1..20}; do curl localhost:9000/foo; done
-for i in {1..30}; do ./curl.sh; done
+for i in {1..5}; do curl localhost:9000/foo; done
+#for i in {1..5}; do ./curl.sh; done
--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,6 +1,6 @@
 package example
 
-import java.util.concurrent.Executors
+import java.util.concurrent.{ExecutorService, Executors, ScheduledExecutorService}
 
 import cats.effect.{ContextShift, ExitCode, IO, IOApp, Timer}
 import cats.implicits._
@@ -8,14 +8,14 @@ import kamon.Kamon
 import kamon.http4s.middleware._
 import kamon.instrumentation.executor.ExecutorInstrumentation
 import kamon.instrumentation.executor.ExecutorInstrumentation.Settings
-import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.dsl.io._
 import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.{HttpRoutes, Request, Uri}
+import org.http4s.HttpRoutes
 import org.slf4j.LoggerFactory
 import org.http4s.implicits._
 
+import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
 
 object Main extends IOApp {
@@ -24,43 +24,40 @@ object Main extends IOApp {
   private val host = "0.0.0.0"
   private lazy val logger = LoggerFactory.getLogger(this.getClass)
 
+  val scheduledES: ScheduledExecutorService = ExecutorInstrumentation.instrumentScheduledExecutor(
+    Executors.newScheduledThreadPool(1),
+    "globalScheduled"
+  )
+
+  private val global: ExecutorService = ExecutorInstrumentation.instrument(
+    Executors.newFixedThreadPool(1),
+    "global",
+    new Settings(shouldTrackTimeInQueue = true, shouldPropagateContextOnSubmit = true)
+  )
   implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(
-    ExecutorInstrumentation.instrument(
-      Executors.newFixedThreadPool(10),
-      "global",
-      new Settings(shouldTrackTimeInQueue = true, shouldPropagateContextOnSubmit = true)
-    )
+    global
   )
   override implicit val contextShift: ContextShift[IO] = IO.contextShift(ec)
-  override implicit val timer: Timer[IO] = IO.timer(ec)
+  override implicit val timer: Timer[IO] = IO.timer(ec, scheduledES)
 
-  override def run(args: List[String]): IO[ExitCode] = BlazeClientBuilder[IO](ec).resource.use { http4sclient =>
-    val cli = client.KamonSupport(http4sclient)
+  override def run(args: List[String]): IO[ExitCode] = {
     val router = Router(
       "/" -> HttpRoutes.of[IO] {
-        case GET -> Root / "bar" / traceId =>
-          logger.info(s"Client: called with traceId $traceId and current is $currentTraceId")
-          Ok(traceId)
         case GET -> Root / "foo" =>
           for {
-            _ <- IO(logger.info(s"Server: calling client with $currentTraceId"))
-            _ <- cli.expect[String](Request[IO](GET, Uri.unsafeFromString(s"http://localhost:9000/bar/$currentTraceId")))
-              .attempt
-              .map { clientResponse =>
-                logger.info(s"Server: client returned: $clientResponse and current is $currentTraceId")
-                clientResponse
-              }
+            _ <- IO(logger.info(s"Server: before ${Kamon.currentSpan().trace.id.string}"))
+            _ <- IO.sleep(1000.millis)
+            _ <- IO(logger.info(s"Server: after ${Kamon.currentSpan().trace.id.string}"))
             r <- Ok(s"GET")
           } yield r
       }
     )
 
-    val configuredHttpService = server.KamonSupport[IO](router, host, port).orNotFound
     for {
       exitCode <- BlazeServerBuilder[IO]
         .bindHttp(port, host)
-        .withExecutionContext(ec) //<-----------------------  IMPORTANT
-        .withHttpApp(configuredHttpService)
+        .withExecutionContext(ec)
+        .withHttpApp(server.KamonSupport[IO](router, host, port).orNotFound)
         .serve
         .compile
         .drain
@@ -68,5 +65,4 @@ object Main extends IOApp {
     } yield exitCode
   }
 
-  private def currentTraceId: String = Kamon.currentSpan().trace.id.string
 }
