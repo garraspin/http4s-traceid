5c47ce97ca2d25467f49bb9093dffde8ca163fcb
Samuel Fernandez Munoz <garraspin00@gmail.com>
2019-12-20 10:36:38 +0100

    Updated versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
-  lazy val http4sVersion = "0.20.11"
+  lazy val http4sVersion = "0.20.15"
   lazy val http4s = Seq(
     "org.http4s"                       %% "http4s-dsl"                  % http4sVersion,
     "org.http4s"                       %% "http4s-blaze-server"         % http4sVersion,
@@ -15,7 +15,7 @@ object Dependencies {
   )
 
   lazy val monitoring = Seq(
-    "io.kamon"                         %% "kamon-bundle"                % "2.0.1",
+    "io.kamon"                         %% "kamon-bundle"                % "2.0.5",
     "io.kamon"                         %% "kamon-http4s"                % "2.0.0"
   )
 }
