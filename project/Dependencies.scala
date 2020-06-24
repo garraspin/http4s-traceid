867d061bde139fba3e8264d51498f7f955bc9138
Samuel Fernandez Munoz <garraspin00@gmail.com>
2020-06-24 14:10:54 +0200

    Updated libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,8 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
-  lazy val http4sVersion = "0.21.1"
+  lazy val http4sVersion = "0.21.4"
+  lazy val kamonBundle = "2.1.1"
   lazy val http4s = Seq(
     "org.http4s"                       %% "http4s-dsl"                  % http4sVersion,
     "org.http4s"                       %% "http4s-blaze-server"         % http4sVersion,
@@ -15,9 +16,9 @@ object Dependencies {
   )
 
   lazy val monitoring = Seq(
-    "io.kamon"                         %% "kamon-bundle"                % "2.0.6",
-    "io.kamon"                         %% "kamon-scala-future"          % "2.0.1",
-    "io.kamon"                         %% "kamon-cats-io"                % "2.0.1",
-    "io.kamon"                         %% "kamon-http4s"                % "2.0.2"
+    "io.kamon"                         %% "kamon-bundle"                % kamonBundle,
+    "io.kamon"                         %% "kamon-scala-future"          % kamonBundle,
+    "io.kamon"                         %% "kamon-cats-io"               % kamonBundle,
+    "io.kamon"                         %% "kamon-http4s"                % "2.0.3"
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,2 @@
-addSbtPlugin("io.kamon" % "sbt-kanela-runner" % "2.0.3")
+addSbtPlugin("io.kamon" % "sbt-kanela-runner" % "2.0.6")
 addSbtPlugin("com.timushev.sbt"  % "sbt-updates"          % "0.3.4")
